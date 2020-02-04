176525650c6fa5fdf08736a3b7186aca0f83d1d9
epixieme <kirstie_hayes@hotmail.com>
2020-02-04 12:43:40 +0000

    updated react components parent child relationship

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,13 @@
 import React,{Component} from 'react'
-const Card =()=>{
+const Card =({name,email,id} )=>{ // could even destructre at a props  level. so replace props with {name,email,id} 
+//meaning we don't have to declare the =props as they are automatciall declared ass props
+	//const {name,email,id} = props;// destructuring - props doesn't ahve to be wrtten each time then below
   return (
-  	<div className='bg-light-green'>
-  	<img  alt='photo' src='https://robohash.org/test?200x200' />
+  	<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+  	<img  alt='photo' src={`https://robohash.org/${id}?200x200`}/> 
   	  <div>
-  	   <h2>Jane Doe</h2>
-  	   <p>Jane.Doe@madeup.com</p>
+  	   <h2>{name}</h2>
+  	   <p>{email}</p>
   	  </div>
   	</div>
    )
--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Card from './Card';
+// import Card from './Card';
+import CardList from './CardList'
 import 'tachyons'
+import {robots} from './robots' // because this file is not using export default and just default we need to use brackets
+                                //for destructuring. if the  robots js had another variable could write it like 
+                                // {robots,cats,mice}
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-	<div>
-	<Card />
-	<Card />
-	<Card />
-	</div>, document.getElementById('root'));
+ReactDOM.render(<CardList robots={robots}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
