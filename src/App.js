a12bc41ddf1db485294ba09b0910f98414c6bf0d
epixieme <kirstie_hayes@hotmail.com>
2020-02-04 16:57:06 +0000

    updated font with sega font

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React, {Component} from 'react';
 import CardList from './CardList'
 import SearchBox from './SearchBox'
 import {robots} from './robots'
-
+import './App.css'
 
 class App extends Component{
 constructor(){
 super()
 this.state={
   robots:robots,
-  searchfield:'' // this is the only area that can chnage
+  searchfield:'' // this is the only area that can chnage but it should not be chnaged directly
+  //instead use setstate
 }
 }
 onSearchChange=(event)=>{ // always use an arrow here on your own funcitonality esel it will error
-this.setState({searchfield: event.target.value})
+this.setState({searchfield: event.target.value}) // this stops state from being modified directly
+// notice we have chnaged the  key searchfield value - changes this field to whatever we type
 }
 
 render(){
@@ -23,7 +25,7 @@ return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
 
 return (
 <div className ='tc'>
-  <h1>RoboFriends</h1>
+  <h1 className ='f1'>RoboFriends</h1>
   <SearchBox SearchChange = {this.onSearchChange}/>
   <CardList robots={filteredRobots}/>
 </div>
