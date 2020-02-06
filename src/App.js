045d35c9dc83480c91819f2e6ae3f5631da076e8
epixieme <kirstie_hayes@hotmail.com>
2020-02-06 14:19:27 +0000

    added users api fetch request

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
 import React, {Component} from 'react';
 import CardList from './CardList'
 import SearchBox from './SearchBox'
-import {robots} from './robots'
 import './App.css'
 
-class App extends Component{
+class App extends Component{// App.js is a smart component as it has state whihc describes the app
+	//and has the class syntax
 constructor(){
-super()
-this.state={
-  robots:robots,
-  searchfield:'' // this is the only area that can chnage but it should not be chnaged directly
+  super() 
+  this.state={
+    robots:[],
+    searchfield:'' // this is the only area that can chnage but it should not be chnaged directly
   //instead use setstate
 }
 }
+componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/users')// fetch is a window object notice it doesnt attacch to anything - means its native
+    .then(response=>response.json())
+    .then(users=>this.setState({robots:users}))
+  }
+ 
+
 onSearchChange=(event)=>{ // always use an arrow here on your own funcitonality esel it will error
-this.setState({searchfield: event.target.value}) // this stops state from being modified directly
+  this.setState({searchfield: event.target.value}) // this stops state from being modified directly
 // notice we have chnaged the  key searchfield value - changes this field to whatever we type
 }
 
 render(){
-const filteredRobots =this.state.robots.filter(robot=>{
-return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+  const filteredRobots =this.state.robots.filter(robot=>{
+    return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
 })
+  if (this.state.robots.length === 0){
+  	  return <h1>Loading...</h1>
+  }else {
 
 return (
-<div className ='tc'>
-  <h1 className ='f1'>RoboFriends</h1>
-  <SearchBox SearchChange = {this.onSearchChange}/>
-  <CardList robots={filteredRobots}/>
-</div>
+  <div className ='tc'>
+    <h1 className ='f1'>RoboFriends</h1>
+    <SearchBox SearchChange = {this.onSearchChange}/>
+    <CardList robots={filteredRobots}/>
+ </div>
   );
   }
-
-
+}
 }
 
 export default App;
